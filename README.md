6f0d90ec3da2505bcffcfe1425fc09398fe02477
Andrew Ferlitsch <Andrew Ferlitsch>
2016-11-13 11:39:24 -0800

    added more howto for webserver to README

--- README.md
+++ README.md
@@ -59,9 +59,48 @@ The Data (Model) endpoint can take the following parameters:
 	type=data-type								# Optional
 	arg=value-to-pass-as-an-argument-to-method	# Optional
 
-#### Example: instantiate a data object of type short, and set its value to 6
+#### Example: Instantiate a data object of type short, and set its value to 6
 
-http://localhost:8080/data?type=short&method=set&arg=6
+	http://localhost:8080/data?type=short&method=set&arg=6
+	
+	Response: {"id":2,"status":200,"content":""}
+	
+#### Example: Get the value of the existing data object
+
+	http://localhost:8080/data?method=get
+	
+	Response: {"id":3,"status":200,"content":"6"}
+	
+#### Example: Parse (invalid) input for data object
+
+	http://localhost:8080/data?method=parse&arg=0x2
+	
+	Response: {"id":4,"status":500,"content":"DataShort.Parse: invalid input: 0x2"}
+	
+### Data State (Model)
+
+The Data State (Model), which is an abstract class extended from the Data (Model) is accessed
+by adding /state to the endpoint path:
+
+	http://localhost:8080/data/state
+	
+#### Example: Instantiate a data state object of type short, and set its value to 6
+
+	http://localhost:8080/data/state?type=short&method=set&arg=6
+	
+	Response: {"id":1,"status":200,"content":""}
+	
+#### Example: Get the value of the existing data object
+
+	http://localhost:8080/data/state?method=get
+	
+	Response: {"id":2,"status":200,"content":"6"}
+	
+#### Example: Parse (valid) input for data object
+
+	http://localhost:8080/data/state?method=parse&arg=0x2
+	
+	Response: {"id":3,"status":200,"content":""}
 
 
 
---
+++ src/main/java/webserver/SchemaController.java
@@ -0,0 +1,51 @@
+package epipog.webserver;
+
+import epipog.schema.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+@RequestMapping("/schema")
+public class SchemaController {
+
+    private final AtomicLong counter = new AtomicLong();
+	private Schema s = null;
+
+    @RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody Response schema( @RequestParam(value="method", required=true)  String method,
+										  @RequestParam(value="type",   required=false) String type,
+										  @RequestParam(value="arg",    required=false) String arg
+									    ) {
+		Long id = counter.incrementAndGet();
+		
+		if ( null != type ) {
+			if ( null == arg )
+				return new Response( id, 500, "Arg is null");
+			switch ( type ) {
+			case "table" 		: s = new SchemaTable( arg ); 	break;
+			case "dynamic"		: s = new SchemaDynamic( arg ); 	break;
+			default				: return new Response( id, 500, "Unknown Type");
+			}
+		}
+		
+		if ( null == s ) return new Response( id, 500, "Schema is Null");
+		
+		String result = "";
+		try {
+			switch ( method ) {
+			case "sets": s.SetS(null); break;
+			default: return new Response( id, "Unknown Method");
+			}
+		} catch ( SchemaException e 		 ) { return new Response( id, 500, e.getMessage() );}
+          
+		return new Response( id, result );
+    }
+
+}
