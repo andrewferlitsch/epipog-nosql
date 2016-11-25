b1d498529089fa7f5e3223dfb559baaead208dc2
Andrew Ferlitsch <Andrew Ferlitsch>
2016-11-25 13:02:44 -0800

    placeholder code for Index

--- src/main/java/Index/Index.java
+++ src/main/java/Index/Index.java
@@ -4,5 +4,27 @@
 package epipog.index;
 
 public interface Index {
-
+	// Method for adding a hashed entry to the index
+	// Return:
+	//	-1 : new entry (not found)
+	//	not -1 : position of found entry
+	public long Add( long hash, long pos );
+	
+	// Method for finding a hashed entry from index
+	// Return
+	//	-1 : not found
+	//	not -1 : found, return position in datastore
+	public long Find( long hash );
+	
+	// Method for removing a hash entry from the index
+	// Return
+	//	-1 : not found
+	//	not -1 : found and removed
+	public long Remove( long hash );
+	
+	// Method to return the position in storage of the nth record (row/document)
+	// Return
+	//	-1 : no such element
+	//  >0 : storage position
+	public long Pos( int nth );
 }
